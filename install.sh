7b738042467d6457551e3fb5040dc65407d7e75c
denstiny <2254228017@qq.com>
2020-03-30 09:49:31 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/denstiny/Terslation.git
-sudo cp Terslation/fanyi.sh /bin/fanyi
+sudo cp terslation/fanyi.sh /bin/fanyi
 sudo chmod +x /bin/Terslation
-sudo cp Terslation/fanyi.py /usr/local/src/fany.py
+sudo cp terslation/fanyi.py /usr/local/src/fany.py
 echo "Before the installation is complete, run, please make sure that whether have dependence"
 echo "There is no problem, please press enter to continue..."
