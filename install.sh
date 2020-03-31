cb5f939e3e172567b79612fb2f163ea98afc4654
denstiny <2254228017@qq.com>
2020-03-31 06:11:55 -0400

    Update non-root users can't one

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 #git clone https://github.com/denstiny/Terslation.git
-<<<<<<< HEAD
-sudo cp translation/fanyi.sh /bin/terlat
-sudo chmod +x /bin/terlat
-sudo mkdir /usr/local/src/fanyi && sudo chmod +777 /usr/local/src/fanyi
-=======
 sudo cp translation/fanyi.sh /bin/terslation
 sudo chmod +x /bin/terslation 
 sudo mkdir /usr/local/src/fanyi && sudo chmod 777 /usr/local/src/fanyi
->>>>>>> bfebe376de34a8e2b530d6f64b2ff172adfd23b2
 sudo cp translation/fanyi.py /usr/local/src/fanyi/fanyi.py
 echo "Before the installation is complete, run, please make sure that wh
 ether have dependence"
