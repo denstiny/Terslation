bfebe376de34a8e2b530d6f64b2ff172adfd23b2
Gnglas <57088952+denstiny@users.noreply.github.com>
2020-03-31 05:47:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #git clone https://github.com/denstiny/Terslation.git
 sudo cp translation/fanyi.sh /bin/terslation
-sudo chmod +x /bin/terslation
-sudo mkdir /usr/local/src/fanyi
+sudo chmod +x /bin/terslation 
+sudo mkdir /usr/local/src/fanyi && sudo chmod 777 /usr/local/src/fanyi
 sudo cp translation/fanyi.py /usr/local/src/fanyi/fanyi.py
 echo "Before the installation is complete, run, please make sure that wh
 ether have dependence"
