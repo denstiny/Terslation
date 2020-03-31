b312717185f9e85cc71ea5664e49ccaa04720031
denstiny <2254228017@qq.com>
2020-03-31 09:03:13 -0400

    使用terlat命令进行翻译

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #git clone https://github.com/denstiny/Terslation.git
-sudo cp translation/fanyi.sh /bin/terslation
-sudo chmod +x /bin/terslation 
+sudo cp translation/fanyi.sh /bin/terlat
+sudo chmod +x /bin/terlat 
 sudo mkdir /usr/local/src/fanyi && sudo chmod 777 /usr/local/src/fanyi
 sudo cp translation/fanyi.py /usr/local/src/fanyi/fanyi.py
 echo "Before the installation is complete, run, please make sure that wh
