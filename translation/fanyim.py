e4c120becd5e2a904ca1b12373736381a96c03ba
denstiny <2254228017@qq.com>
2020-12-03 22:13:48 +0800

    updata terlat list

--- translation/fanyim.py
+++ translation/fanyim.py
@@ -2,7 +2,10 @@
 # coding=utf-8
 import requests
 import re
+import os
 import sys
+#获取家目录路径
+path = os.environ['HOME'] + "/.terlist/"
 
 
 def main():
@@ -10,9 +13,12 @@ def main():
     URL = "http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule"
     resStr = '\"tgt\"\:\"(.*?)\"\}'
     Strend = ""
-    Str = ""
-    print(type(Str))
-    Str = input()
+    #获取标准缓冲区
+    Str = " ".join(sys.argv)[10:]
+    if len(sys.argv) >= 2:
+        if len(re.findall(".*? -l$", " ".join(sys.argv))) == 1:
+            os.system("tac ~/.terlist/.terlist | less")
+            return
 
     data = {"i": Str, "from": "AUTO", "to": "AUTO", "doctype": "json"}
     res = requests.post(url=URL, data=data)
@@ -21,7 +27,18 @@ def main():
     for st in StrJson:
         Strend += st
     print("InputStr = " + Str + "\nTerlat   = " + Strend)
-    pass
+    if (len(sys.argv)) == 2:
+        if ord(list(Str)[0]) > 1000 or ord(list(Strend)[0]) > 1000:
+            Terlist(Str, Strend)
+
+
+def Terlist(a, b):
+    with open(path + ".terlist") as f:
+        fc = f.read()
+        if len(re.findall(a, fc)) < 1:
+            with open(path + ".terlist", "a+") as f:
+                f.write(a + " " + b + '\n')
+                print(a + " " + b + " 加入生僻字")
 
 
 main()
