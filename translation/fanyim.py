04686048877dc70969cdbccddaa843503ce033b3
denstiny <2254228017@qq.com>
2022-10-14 18:56:48 +0800

    fead: init

--- translation/fanyim.py
+++ translation/fanyim.py
@@ -5,9 +5,18 @@ import pyperclip
 import re
 import os
 import sys
+import json
 #获取家目录路径
-path = os.environ['HOME'] + "/.terlist/"
+path = os.environ['HOME'] + "/.terlist"
 
+def readConfig():
+    file_path = os.environ['HOME'] + "/.config/ters.json"
+    if os.path.exists(file_path):
+        with open(file_path,mode='r',encoding='utf-8') as f:
+            json_data = json.load(f)
+            return json_data
+    else:
+        return {}
 
 def main():
 
@@ -22,13 +31,17 @@ def main():
             os.system("tac ~/.terlist/.terlist | less")
             return
 
-    data = {"i": Str, "from": "AUTO", "to": "AUTO", "doctype": "json"}
+    conf = readConfig()
+    if conf == {}:
+        data = {"i": Str, "from": "AUTO", "to": "AUTO", "doctype": "json"}
+    else:
+        data = {"i": Str, "from": conf['from'], "to": conf['to'], "doctype": "json"}
     res = requests.post(url=URL, data=data)
     res = res.content.decode("utf-8")
     StrJson = re.findall(resStr, res, re.S)
     for st in StrJson:
         Strend += st
-    print("InputStr = " + Str + "\nTerlat   = " + Strend)
+    print("FROM = " + Str + "\nTO   = " + Strend)
     str = "echo \"" + Strend + "\" | xclip -selection clipboard"
     os.system(str)
     if (len(sys.argv)) == 2 and ord(list(Str)[0]) < 1000:
@@ -39,15 +52,15 @@ def main():
 def Terlist(a, b):
     if not os.path.exists(path):
         os.mkdir(path)
-        f = open(path + "terlist","w")
+        f = open(path + "/terlist","w")
         f.close()
-    with open(path + "terlist") as f:
-        fc = f.read()
-        if len(re.findall(a, fc)) < 1:
-            with open(path + "terlist", "a+") as f:
-                f.write(a + " " + b + '\n')
-                print("加入生僻字>>  " + a + ":" + b)
-                
+    else:
+        with open(path + "/terlist") as f:
+            fc = f.read()
+            if len(re.findall(a, fc)) < 1:
+                with open(path + "/terlist", "a+") as f:
+                    f.write(a + " " + b + '\n')
+                    print("加入生僻字>>  " + a + ":" + b)
 
 
 main()
